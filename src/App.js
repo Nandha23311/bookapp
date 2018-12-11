1417c1b3809e4f127a70098c63c3bf3228f3dfa9
Nandha <nanda@flowerpotmobility.com>
2018-12-12 00:14:18 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import SignUpAndLogin from './signupAndLogin'
+
+class App extends Component {
+  render() {
+    return (
+      <SignUpAndLogin />
+    );
+  }
+}
+
+export default App;
---
+++ src/signupAndLogin.js
@@ -0,0 +1,154 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class SignUpAndLogin extends Component {
+  constructor() {
+    super();
+    this.state = {
+        loginUserName: "",
+        userName: "",
+        loginPassword: "",
+        password: "",
+        fullName: "",
+        mobileNumber: ""
+      }
+  }
+  handleLogin(){
+    let reqBody = {
+      userName: this.state.loginUserName,
+      password: this.state.loginPassword
+    }
+    console.log("ReqBody --> ",reqBody)
+    alert("Login")
+  }
+  handleSignup(){
+    let reqBody = {
+      userName: this.state.userName,
+      password: this.state.password,
+      fullName: this.state.fullName,
+      mobileNumber: this.state.mobileNumber
+    }
+    console.log("ReqBody --> ",reqBody)
+    alert("Signup")
+  }
+  handleFullName(event){
+    this.setState({ fullName: event.target.value })
+  }
+  handleUserName(event){
+    this.setState({ userName: event.target.value })
+  }
+  handlePassword(event){
+    this.setState({ password: event.target.value })
+  }
+  handleMobileNumber(event){
+    this.setState({ mobileNumber: event.target.value })
+  }
+  handleLoginUserName(event){
+    this.setState({ loginUserName: event.target.value })
+  }
+  handleLoginPassword(event){
+    this.setState({ loginPassword: event.target.value })
+  }
+  render() {
+    return (
+        <div>
+          <div className= "full_screen">
+
+            <div className= "top">
+              <center><h1> Kalki Books Shop</h1></center>
+            </div>
+            <div className= "body">
+              <div className= "body_left">
+                <center><img className= "book_img" src="https://book.ponniyinselvan.in/assets/vikatan_edition.jpg" alt="PONNIYIN SELVAN" ></img></center>
+              </div>
+              <div className= "body_right">
+
+                <div className= "body_right_top">
+                  <center><h3>Login</h3></center>
+                  <form onSubmit={this.handleLogin.bind(this)} >
+                    <div className= "curved_view">
+                      <input
+                        className="input_with_rounded"
+                        type="text"
+                        placeholder="Username"
+                        value = {this.state.loginUserName}
+                        onChange = {this.handleLoginUserName.bind(this)}
+                      />
+                      <input
+                        className="input_with_rounded"
+                        type="text"
+                        placeholder="Password"
+                        style={{ marginTop: "5px" }}
+                        value = {this.state.loginPassword}
+                        onChange = {this.handleLoginPassword.bind(this)}
+                      />
+                      <button
+                        type="submit"
+                        value="Login"
+                        className="button_with_rounded"
+                      >
+                      Submit
+                    </button>
+                    </div>
+                  </form>
+                </div>
+                <h4 style={{textAlign:"center"}}>or</h4>
+                <div className= "body_right_bottom">
+                  <center><h3>Signup</h3></center>
+                  <form onSubmit={this.handleSignup.bind(this)} >
+                  <div className= "curved_view">
+                      <input
+                        className="input_with_rounded"
+                        type="text"
+                        placeholder="Fullname"
+                        value = {this.state.fullName}
+                        onChange = {this.handleFullName.bind(this)}
+                      />
+                      <input
+                        className="input_with_rounded"
+                        type="text"
+                        placeholder="Mobilenumber"
+                        style={{ marginTop: "5px" }}
+                        value = {this.state.mobileNumber}
+                        onChange = {this.handleMobileNumber.bind(this)}
+                      />
+                      <input
+                        className="input_with_rounded"
+                        type="text"
+                        placeholder="Username"
+                        value = {this.state.userName}
+                        onChange = {this.handleUserName.bind(this)}
+                      />
+                      <input
+                        className="input_with_rounded"
+                        type="text"
+                        placeholder="Password"
+                        style={{ marginTop: "5px" }}
+                        value = {this.state.password}
+                        onChange = {this.handlePassword.bind(this)}
+                      />
+                      <button
+                        type="submit"
+                        value="Login"
+                        className="button_with_rounded"
+                      >
+                      Submit
+                    </button>
+                    </div>
+                  </form>
+                </div>
+
+              </div>
+            </div>
+            <div>
+              <h2> Botttom</h2>
+            </div>
+
+          </div>
+        </div>
+    );
+  }
+}
+
+export default SignUpAndLogin;
